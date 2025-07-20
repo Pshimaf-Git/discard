df7112ca82ada1b1184d83261778b797ced878dd
psimaf <psimaf27@gmail.com>
2025-07-20 18:56:59 +0300

    add test case which use Discard in another structs for read/write op(bufio.Reader, bufio.Writer, bufio.Scaner, io.OffsetWriter and io.MultiWriter)

--- discard_test.go
+++ discard_test.go
@@ -1,10 +1,12 @@
 package discard
 
 import (
+	"bufio"
 	"io"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func isEOF(err error) bool {
@@ -123,4 +125,43 @@ func TestClose(t *testing.T) {
 	d := New()
 	err := d.Close() // Closing should not cause an error
 	assert.NoError(t, err, "Close should not return an error")
+}
+
+func TestDiscard(t *testing.T) {
+	d := New()
+	require.NotNil(t, d, "Discard should return a non-nil Discard instance")
+
+	reader := bufio.NewReader(d)
+	writer := bufio.NewWriter(d)
+
+	// Test reading from Discard
+	_, err := reader.Read(make([]byte, 10))
+	assert.Truef(t, isEOF(err), "Reading from Discard should return io.EOF")
+
+	_, err = reader.ReadString('\n')
+	assert.Truef(t, isEOF(err), "Reading from Discard with ReadString should return io.EOF")
+
+	// Test writing to Discard
+	s := "test"
+	n, err := writer.Write([]byte(s))
+	assert.Equal(t, len(s), n, "Writing to Discard should return the length of the input")
+	assert.NoError(t, err, "Writing to Discard should not return an error")
+
+	err = writer.Flush()
+	assert.NoError(t, err, "Flushing Discard writer should not return an error")
+
+	limitR := io.LimitReader(d, 100)
+	_, err = limitR.Read(make([]byte, 10))
+	assert.Truef(t, isEOF(err), "Reading from LimitReader of Discard should return io.EOF")
+
+	limitW := io.NewOffsetWriter(d, 1)
+	n, err = limitW.Write([]byte(s))
+	assert.Equal(t, len(s), n, "Writing to OffsetWriter of Discard should return the length of the input")
+	assert.NoError(t, err, "Writing to OffsetWriter of Discard should not return an error")
+
+	// Test MultiWriter with Discard
+	mult := io.MultiWriter(d, New(), New(), New())
+	n, err = mult.Write([]byte(s))
+	assert.Equal(t, len(s), n, "Writing to MultiWriter of Discard should return the length of the input")
+	assert.NoError(t, err, "Writing to MultiWriter of Discard should not return an error")
 }
